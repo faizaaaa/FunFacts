b36f97e9662a3b2de3fdfbd50d3e444617c4a98c
faizaaaa <ffazal01@dcs.bbk.ac.uk>
2021-06-20 21:31:25 +0100

    choosing a random fact

--- app/src/main/java/com/example/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/funfacts/FunFactsActivity.java
@@ -3,12 +3,50 @@ package com.example.funfacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
-
+// Declare our view variables
+    private TextView factTextView;
+    private Button showFactButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign the Views from the layout file to corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+            // The button was clicked so update the factTextView with a new fact.
+                //Randomly select a fact
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(10);
+                String fact = randomNumber+"";
+
+                //Update the screen with our new fact.
+                factTextView.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
     }
 }
